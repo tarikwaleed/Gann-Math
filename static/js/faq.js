7852b394b188519f9c8b28b988c1b21b380f8d0f
tarikwaleed <tarikwaleed.tech@gmail.com>
2023-07-11 02:36:08 +0300

    addedd simple FAQ button that shows a popup in the middle of the screen

---
+++ static/js/faq.js
@@ -0,0 +1,8 @@
+// popup.js
+$(document).ready(function () {
+  // Attach a click event handler to the button
+  $('#popupButton').click(function () {
+    // Show the popup container
+    $('#popupContainer').show();
+  });
+});
