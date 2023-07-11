069259f10a505d8478e167349d665c860aa6b997
tarikwaleed <tarikwaleed.tech@gmail.com>
2023-07-11 03:28:34 +0300

    added the toggle functionality

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,8 +1,12 @@
-// popup.js
 $(document).ready(function () {
-  // Attach a click event handler to the button
   $('#popupButton').click(function () {
-    // Show the popup container
-    $('#popupContainer').show();
+    // Check the current visibility of the popup container
+    if ($('#popupContainer').is(':visible')) {
+      // If the container is visible, hide it
+      $('#popupContainer').hide();
+    } else {
+      // If the container is hidden, show it
+      $('#popupContainer').show();
+    }
   });
 });
